4b9e711869fd79de96bda35014f1bf4e83c2e77e
Trum <72457701+Trum-ok@users.noreply.github.com>
2023-04-18 00:59:43 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+import sqlite3
+
+# import pandas as pd
+# from sqlalchemy import create_engine
+#
+# data = pd.read_excel('templates/static/tables/dasabate.xlsx')
+#
+# engine = create_engine('sqlite:///exhibits.db', echo=True)
+# sqlite_connection = engine.connect()
+#
+# data.to_sql('exhibits', sqlite_connection, if_exists='replace', index=False)
+
+# conn = sqlite3.connect('exhibits.db')  # database connection
+# c = conn.cursor()
+
+app = Flask(__name__, template_folder='templates', static_folder="templates/static")
+
+
+def get_data():
+    conn = sqlite3.connect('exhibits.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM exhibits')
+    data = cur.fetchall()
+    conn.close()
+    return data
+
+
+@app.route('/')
+@app.route("/index/")
+@app.route("/index.html/")
+def index():
+    return render_template('/index.html')
+
+
+@app.route('/')
+@app.route("/table/")
+@app.route("/table.html/")
+def table():
+    data = get_data()
+    # print(data)
+    return render_template('/table.html', data=data)
+
+
+@app.route('/')
+@app.route("/contacts/")
+@app.route("/contacts.html/")
+def contacts():
+    return render_template('/contacts.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
